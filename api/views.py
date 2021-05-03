3ff5bfd3d931449471b2ea6970efdedcbf8ab392
Sreebash Kumar Das <sreebashdas.1@gmail.com>
2021-05-03 21:37:15 +0600

    Init

---
+++ api/views.py
@@ -0,0 +1,59 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api.models import Task
+from api.serializers import TaskSerializer
+
+
+@api_view(['GET'])
+def api_over_view(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response('Item successfully deleted!')
